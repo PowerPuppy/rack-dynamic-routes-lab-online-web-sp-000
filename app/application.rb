3265cf212e94d3de1969cf96c78d2fc12966589c
Chris Cheong <chrisxcheong@gmail.com>
2020-01-12 12:31:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   @@items = []
   def call(env)
     resp = Rack::Respond.new
-    req = Rack::Request.new
+    req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
